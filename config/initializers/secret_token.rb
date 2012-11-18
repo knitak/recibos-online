a9f2c85c8fcfda4e0be8119d9ee57b15909a93e5
Ana Pedrosa <anacpedrosa@gmail.com>
2012-11-18 01:39:35 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RecibosOnline::Application.config.secret_token = '132f3fc9ed28aff32590a46a1029b16ecf05c1207740431f0e7452259a7cb58d997b34c868b36cc7f247d197f3fbeabb08842b07b56abf8cf1905b54dd1982dc'
